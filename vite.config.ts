8201df82ab738bae5ae14e653ed1906fe4baa324
Nat1an <nathan@nat1an.xyz>
2025-05-18 04:40:38 +0000

    fix(vite.config.ts): correct base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "ros-marinus-program-construction",
+  base: "/ros-marinus-program-construction",
   plugins: [react()],
-})
+});
